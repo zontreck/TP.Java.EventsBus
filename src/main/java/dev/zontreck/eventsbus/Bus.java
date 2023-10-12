783de1e9c00e72611692910816e9d0de2b7fdb6d
Zontreck <tarapiccari@gmail.com>
2023-10-12 15:18:06 -0700

    Start implementing bus.

--- src/main/java/dev/zontreck/eventsbus/Bus.java
+++ src/main/java/dev/zontreck/eventsbus/Bus.java
@@ -1,6 +1,10 @@
 package dev.zontreck.eventsbus;
 
-public class Bus
-{
+public class Bus {
+    public static Bus Main = new Bus("Main Event Bus");
+    public final String BusName;
 
+    public Bus(String name) {
+        BusName = name;
+    }
 }
